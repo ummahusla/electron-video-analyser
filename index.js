d7fdae40c954e09d6f3534e3440615550fe9a143
Edvins Antonovs <ummahusla@gmail.com>
2017-09-21 21:45:45 +0100

    Get the subbmited video meta data and pass it to frontend

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
 
 app.on("ready", () => {
 	console.log("App is now ready");
-	const mainWindow = new BrowserWindow({});
+	mainWindow = new BrowserWindow({});
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on("video:submit", (event, path) => {
+	ffmpeg.ffprobe(path, (err, metadata) => {
+		mainWindow.webContents.send("video:metadata", metadata.format.duration);
+	});
 });
